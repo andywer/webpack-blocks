4f01b3589c85450476ffbd845f96ec078e8a7823
Andy Wermke <andy@dev.next-step-software.com>
2016-10-12 20:51:09 +0200

    Add test for `env()`

--- packages/webpack/__tests__/create-webpack-config.test.js
+++ packages/webpack/__tests__/create-webpack-config.test.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import { createConfig, entryPoint, setOutput, sourceMaps } from '../index'
+import { createConfig, customConfig, entryPoint, env, setOutput, sourceMaps } from '../index'
 import babel from '@webpack-blocks/babel6'
 import cssModules from '@webpack-blocks/css-modules'
 import devServer from '@webpack-blocks/dev-server'
@@ -78,4 +78,41 @@ test('complete webpack config creation', (t) => {
   t.deepEqual(Object.keys(webpackConfig).sort(), [
     'devServer', 'devtool', 'entry', 'module', 'output', 'plugins', 'resolve'
   ])
+})
+
+test('env()', (t) => {
+  const originalEnv = process.env.NODE_ENV
+
+  const create = () => createConfig([
+    env('development', [
+      sourceMaps()
+    ]),
+    env('production', [
+      customConfig({
+        foo: 'bar'
+      })
+    ])
+  ])
+
+  try {
+    process.env.NODE_ENV = ''
+    const defaultConfig = create()
+
+    t.is(defaultConfig.devtool, 'cheap-module-source-map')
+    t.is(defaultConfig.foo, undefined)
+
+    process.env.NODE_ENV = 'development'
+    const devConfig = create()
+
+    t.is(devConfig.devtool, 'cheap-module-source-map')
+    t.is(devConfig.foo, undefined)
+
+    process.env.NODE_ENV = 'production'
+    const prodConfig = create()
+
+    t.is(prodConfig.devtool, undefined)
+    t.is(prodConfig.foo, 'bar')
+  } finally {
+    process.env.NODE_ENV = originalEnv
+  }
 })
