440691796dd28f4a604cf6af433bd4e5508df2d9
Andy Wermke <andy@dev.next-step-software.com>
2016-12-09 13:23:56 +0100

    Minor changes to the `entryPoint()` normalization

--- packages/webpack/index.js
+++ packages/webpack/index.js
@@ -66,6 +66,10 @@ function createBaseConfig (fileTypes) {
 }
 
 /**
+ * Adds one or multiple entry points. If the parameter is not an object the
+ * entry point(s) will be added to the default chunk named `main`.
+ *
+ * @param {object|string[]|string} entry
  * @see https://webpack.github.io/docs/configuration.html#entry
  */
 function entryPoint (entry) {
@@ -75,20 +79,24 @@ function entryPoint (entry) {
 }
 
 function normalizeEntry (entry) {
-  if (typeof entry === 'object') {
-    if (Array.isArray(entry)) {
-      return { main: entry }
-    } else {
-      Object.keys(entry).forEach(function normalizeNamedEntry (entryName) {
-        if (!Array.isArray(entry[entryName])) {
-          entry[entryName] = [entry[entryName]]
-        }
-      })
-      return entry
+  if (Array.isArray(entry)) {
+    return {
+      main: entry
     }
+  } else if (typeof entry === 'string') {
+    return {
+      main: [ entry ]
+    }
+  } else if (typeof entry === 'object') {
+    Object.keys(entry).forEach(function normalizeNamedEntry (entryName) {
+      if (!Array.isArray(entry[entryName])) {
+        entry[entryName] = [entry[entryName]]
+      }
+    })
+    return entry
+  } else {
+    throw new Error(`Expected entry point to be object, array or string. Instead got: ${entry}`)
   }
-
-  return { main: [entry] }
 }
 
 /**
