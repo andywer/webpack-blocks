e791af1a874cc70ed7eb64eefdc3354f7fe2379c
Andy Wermke <andy@dev.next-step-software.com>
2016-12-11 15:31:39 +0100

    Add sass/extract-text e2e test

--- packages/webpack/__tests__/end-to-end.test.js
+++ packages/webpack/__tests__/end-to-end.test.js
@@ -34,6 +34,20 @@ test('building the babel/postcss/extract-text project works', async (t) => {
   t.true(removeWhitespaces(styleContents).indexOf(removeWhitespaces('.app { margin: 40px; }')) > -1)
 })
 
+test('building the sass/extract-text project works', async (t) => {
+  const projectPath = path.join(fixturesPath, 'sass-extract-text')
+  const buildPath = path.join(projectPath, 'build')
+
+  const config = require(path.join(projectPath, 'webpack.config.js'))
+  await runWebpack(config)
+
+  require(path.join(buildPath, 'bundle.js'))
+
+  // Check if CSS file contains correct content
+  const styleContents = await fs.readFile(path.join(buildPath, 'styles.css'), { encoding: 'utf8' })
+  t.true(removeWhitespaces(styleContents).indexOf(removeWhitespaces('body { padding: 25px; }')) > -1)
+})
+
 function runWebpack (config) {
   return new Promise((resolve, reject) => {
     webpack(config, (error, stats) => {
