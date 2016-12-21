3dde7ef50c0ff78a8468462f06808090e05ede61
Andy Wermke <andy@dev.next-step-software.com>
2016-12-21 21:59:29 +0100

    Add unit tests to babel6 block

---
+++ packages/babel6/__tests__/babel6.test.js
@@ -0,0 +1,40 @@
+import test from 'ava'
+import babel6 from '../index'
+
+test('Babel default options work', (t) => {
+  const block = babel6()
+
+  const context = {
+    fileType: () => '*.js'
+  }
+
+  t.deepEqual(block(context), {})
+  t.deepEqual(block.post(context).module.loaders, [
+    {
+      test: '*.js',
+      exclude: [ /\/node_modules\// ],
+      loaders: [ 'babel-loader?{"cacheDirectory":true}' ]
+    }
+  ])
+})
+
+test('Babel options and exclude work', (t) => {
+  const block = babel6({
+    exclude: 'foo/',
+    presets: ['es2015'],
+    plugins: ['bar']
+  })
+
+  const context = {
+    fileType: () => '*.js'
+  }
+
+  t.deepEqual(block(context), {})
+  t.deepEqual(block.post(context).module.loaders, [
+    {
+      test: '*.js',
+      exclude: [ 'foo/' ],
+      loaders: [ 'babel-loader?{"cacheDirectory":true,"plugins":["bar"],"presets":["es2015"]}' ]
+    }
+  ])
+})
