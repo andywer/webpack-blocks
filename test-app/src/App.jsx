9f79ff9df1c4639cb6636f4f6047b91854f52b3c
Andy Wermke <a.wermke@gmail.com>
2017-01-04 11:02:36 +0100

    Fix indentation to 2 spaces

--- test-app/src/App.jsx
+++ test-app/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styles from './App.css'
 
 const App = () => 
-	<div className={styles.app}>
-		No content here. We only test the build process 😉
-	</div>
+  <div className={styles.app}>
+    No content here. We only test the build process 😉
+  </div>
 
 export default App
