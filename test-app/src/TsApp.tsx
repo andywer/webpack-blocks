6b76a050b32cd06cedd04d80f453157bb64a5846
SuperManitu <supermanitu@gmail.com>
2017-01-18 10:44:54 +0100

    add TsApp component to test typescript build

---
+++ test-app/src/TsApp.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+const styles = require('./App.css')
+
+const TsApp : (() => JSX.Element) = () => 
+  <div className={styles.app}>
+    No content here. We only test the build process 😉
+  </div>
+
+export default TsApp
--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import TsApp from './TsApp'
 
 const mountPoint = document.getElementById('root')
-ReactDOM.render(<App />, mountPoint)
+ReactDOM.render(<div><App /><TsApp /></div>, mountPoint)
