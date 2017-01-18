7fb37b4fe05343c672844d70a587efce95b67a0d
SuperManitu <supermanitu@gmail.com>
2017-01-18 11:22:32 +0100

    drop JSX Component

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import TsApp from './TsApp'
 
 const mountPoint = document.getElementById('root')
-ReactDOM.render(<div><App /><TsApp /></div>, mountPoint)
+ReactDOM.render(<div><App /></div>, mountPoint)
