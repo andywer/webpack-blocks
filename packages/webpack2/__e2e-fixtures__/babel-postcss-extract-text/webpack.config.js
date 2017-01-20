3727581421b67d555c5a83ba0de9733f3787ff4a
Andy Wermke <andy@dev.next-step-software.com>
2017-01-20 16:58:26 +0100

    Use `defineConstants` in webpack2 e2e test like in the webpack 1.x one

--- packages/webpack2/__e2e-fixtures__/babel-postcss-extract-text/webpack.config.js
+++ packages/webpack2/__e2e-fixtures__/babel-postcss-extract-text/webpack.config.js
@@ -1,12 +1,11 @@
 const webpack2 = require('../../index')
 
 // Need to write it like this instead of destructuring so it runs on Node 4.x w/o transpiling
-const addPlugins = webpack2.addPlugins
 const createConfig = webpack2.createConfig
+const defineConstants = webpack2.defineConstants
 const entryPoint = webpack2.entryPoint
 const performance = webpack2.performance
 const setOutput = webpack2.setOutput
-const webpack = webpack2.webpack
 
 const babel = require('@webpack-blocks/babel6')
 const postcss = require('@webpack-blocks/postcss')
@@ -33,9 +32,7 @@ module.exports = createConfig([
     maxEntrypointSize: 500000,
     hints: 'error'
   }),
-  addPlugins([
-    new webpack.DefinePlugin({
-      'process.env': { TEST: '"This is the injected process.env.TEST!"' }
-    })
-  ])
+  defineConstants({
+    'process.env.TEST': 'This is the injected process.env.TEST!'
+  })
 ])
