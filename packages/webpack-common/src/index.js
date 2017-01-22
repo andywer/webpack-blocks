594672eb67360de6dbdd4742857fbba3509fe60c
Andy Wermke <andy@dev.next-step-software.com>
2017-01-22 12:14:03 +0100

    Implement `parseVersion()` in `webpack-common`

--- packages/webpack-common/src/index.js
+++ packages/webpack-common/src/index.js
@@ -1,4 +1,5 @@
 export { default as defineConstants } from './defineConstants'
 export { default as devServer } from './devServer'
+export { default as parseVersion } from './parseVersion'
 export * from './extractText'
 export * from './webpack'
