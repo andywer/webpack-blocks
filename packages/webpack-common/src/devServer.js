6429f8ed449b7d89751c87a64cd0858dfb6a1ef6
Brandon Konkle <brandon@ecliptic.io>
2017-03-05 16:47:18 -0700

    Add NamedModulesPlugin when HotReloading is active
    This is *very* handy for debugging. 😀

--- packages/webpack-common/src/devServer.js
+++ packages/webpack-common/src/devServer.js
@@ -49,7 +49,8 @@ function postConfig (context, config) {
     }, context.devServer.options),
     entry: addDevEntryToAll(config.entry || {}, entryPointsToAdd),
     plugins: [
-      new context.webpack.HotModuleReplacementPlugin()
+      new context.webpack.HotModuleReplacementPlugin(),
+      new context.webpack.NamedModulesPlugin()
     ]
   }
 }
