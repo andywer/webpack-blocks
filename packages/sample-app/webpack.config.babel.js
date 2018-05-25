c6b49836e3f006b9a14baccb066544da62cf36cb
Vlad Zhukov <work4vlad@gmail.com>
2018-05-25 11:52:22 +0300

    Fix a skipped test

--- packages/sample-app/webpack.config.babel.js
+++ packages/sample-app/webpack.config.babel.js
@@ -20,7 +20,6 @@ const {
 } = require('webpack-blocks')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
 const path = require('path')
 
 const developmentConfig = () =>
@@ -35,32 +34,30 @@ const developmentConfig = () =>
       // Increase performance budget thresholds for development mode
       maxAssetSize: 1500000,
       maxEntrypointSize: 1500000
-    })
+    }),
+    css.modules()
   ])
 
 const productionConfig = () =>
   group([
-    extractText(),
-    addPlugins([
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      })
-    ])
+    css.modules({ minimize: true }),
+    extractText('css/[name].[contenthash:hex:8].css')
   ])
 
 module.exports = createConfig([
   setMode(process.env.NODE_ENV || 'development'),
   babel(),
   typescript({ configFileName: path.resolve(__dirname, './tsconfig.json') }),
-  css.modules(),
   addPlugins([
     new HtmlWebpackPlugin({
       inject: true,
       template: './index.html'
     })
   ]),
-  env('development', [entryPoint('./src/index.dev.js'), developmentConfig()]),
+  env('development', [
+    entryPoint('./src/index.dev.js'),
+    developmentConfig()
+  ]),
   env('production', [
     entryPoint('./src/index.js'),
     setOutput('./build/bundle.js'),
