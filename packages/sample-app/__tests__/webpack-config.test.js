1b14ead6c0fef127323c48d6c59c961516235d0c
Marco Fugaro <marco.fugaro@gmail.com>
2018-08-29 21:02:33 +0200

    Reenable css minification in sampleapp (#292)

--- packages/sample-app/__tests__/webpack-config.test.js
+++ packages/sample-app/__tests__/webpack-config.test.js
@@ -107,8 +107,7 @@ test('it builds', async t => {
 
     const css = fs.readFileSync(path.resolve(buildLocation, 'css', files[0]), { encoding: 'utf8' })
     t.true(css.startsWith('.'))
-    t.true(css.includes('margin: 30px auto;'))
-    t.true(css.includes('text-align: center;'))
+    t.true(css.endsWith('{margin:30px auto;text-align:center}'))
   }
 })
 
