662307d81f5d16ad8adb8477ce63d846875b2456
Vlad Zhukov <me@vladzhukov.ru>
2018-09-30 16:51:37 +0300

    Upgrade a few deps

--- packages/webpack/__e2e-fixtures__/postcss-sass-sourcemaps/webpack.config.js
+++ packages/webpack/__e2e-fixtures__/postcss-sass-sourcemaps/webpack.config.js
@@ -2,15 +2,15 @@ const { createConfig, entryPoint, setMode, setOutput } = require('../../index')
 
 const path = require('path')
 const postcss = require('@webpack-blocks/postcss')
-const precss = require('precss')
 const sass = require('@webpack-blocks/sass')
+const postcssVars = require('postcss-simple-vars')
 
 module.exports = createConfig([
   setMode('development'),
   entryPoint(path.join(__dirname, 'styles.sass')),
   setOutput(path.join(__dirname, 'build/bundle.js')),
   postcss({
-    plugins: [precss]
+    plugins: [postcssVars]
   }),
   sass({ indentSyntax: true, sourceMap: true })
 ])
